2a6406415d2731c17f8b2656d7b9709904fbac47
Wren <grahamgrasley@gmail.com>
2020-08-12 15:12:03 -0400

    more comments, code trimming

--- addons/messagebox/message_box.gd
+++ addons/messagebox/message_box.gd
@@ -12,21 +12,24 @@ export(String) var accelerate_action = ""
 export(Array, AudioStream) var voice = []
 export(NodePath) var player = "" setget _set_player
 
-var _isready = false
-var _speed_mult = 1
-var _last_speed = 1
-var _done = false
-var _last_char = 0
-var _accel = false
-var _cool = true
-var _player = null
+var _isready: bool = false
+var _speed_mult: float = 1
+var _last_speed: float = 1
+var _done: bool = false
+var _last_char: int = 0
+var _accel: bool = false
+var _cool: bool = true
+var _player: Node = null
 #github 37720
-var _line_size = 1
-var _max_lines = 1
+var _line_size: int = 1
+var _max_lines: int = 1
 #end hacks
-onready var _tween = Tween.new()
-onready var _old_theme = theme
-onready var _cooldown = Timer.new()
+onready var _tween: Tween = Tween.new()
+onready var _old_theme: Theme = theme
+onready var _cooldown: Timer = Timer.new()
+
+# none of the functions here return anything,
+# so I didn't bother with return typing
 
 ### main loop ###
 
@@ -90,27 +93,27 @@ func _process(delta):
 	if Engine.editor_hint:
 		return
 	
-	#recalculate sizing when theme is changed
+	# recalculate sizing when theme is changed
 	if theme != _old_theme:
 		_old_theme = theme
 		_resized()
 	
-	#scroll up or down to show current text
+	# scroll up or down to show current text
 	if get_visible_line_count() > _max_lines and visible_characters > 0 and speed > 0:
 		_scroll(3)
 	if get_visible_line_count() <= _max_lines and visible_characters > 0 and speed < 0:
 		_scroll(-3)
 	
-	#inline speed multiplier change
+	# inline speed multiplier change
 	if _speed_mult != _last_speed:
 		_last_speed = _speed_mult
 		_tween.playback_speed = _speed_mult * speed
 	
-	#detects when a reversed type is finished
+	# detects when a reversed type is finished
 	if !_done and speed < 0 and _tween.tell() == 0:
 		_on_done()
 	
-	#plays a voice sound when a new letter is typed
+	# plays a voice sound when a new letter is typed
 	if _last_char != visible_characters and voice.size() > 0 and _player != null:
 		_last_char = visible_characters
 		_player.stop()
@@ -119,7 +122,7 @@ func _process(delta):
 
 ### Setgets ###
 
-func _set_player(path):
+func _set_player(path: NodePath):
 	player = path
 	
 	if !_isready:
@@ -130,7 +133,7 @@ func _set_player(path):
 		_player.autoplay = false
 
 
-func _set_speed(val):
+func _set_speed(val: float):
 	speed = float(val)
 	
 	if _tween == null:
@@ -144,7 +147,7 @@ func _set_speed(val):
 		_on_done()
 
 
-func _set_accel(val):
+func _set_accel(val: float):
 	if val <= 0:
 		return
 	
@@ -153,10 +156,7 @@ func _set_accel(val):
 		_tween.playback_speed = _speed_mult * speed * acceleration
 
 
-func _set_message(val):
-	if typeof(val) != TYPE_STRING:
-		return
-	
+func _set_message(val: String):
 	bbcode_text = val
 	message = val
 	if _isready:
@@ -179,11 +179,11 @@ func _resized():
 
 ### other ###
 
-func _scroll(v):
+func _scroll(v: float):
 	get_v_scroll().value += v
 
 
-func _block_speed(val):
+func _block_speed(val: float):
 	if val > 0:
 		_speed_mult = val
 
@@ -212,25 +212,35 @@ func _start_msg():
 
 
 class speedbb extends RichTextEffect:
-	var bbcode = "spd"
-	var caller = null
+	var bbcode: String = "spd"
+	var caller: Node = null
 	
-	func _process_custom_fx(char_fx):
+	func _process_custom_fx(char_fx) -> bool:
 		if Engine.editor_hint:
 			return true
+		
+		# main loop
 		if char_fx.visible and caller != null and char_fx.env.has(""):
+			# first char of speed sequence
 			if char_fx.relative_index == 0 and (
 					(caller.speed >= 0 and caller.percent_visible < 1.0)
 					 or caller.speed < 0):
-				if !char_fx.env.has("_base"):
-					char_fx.env["_base"] = caller._speed_mult
+				
 				caller._block_speed(char_fx.env[""])
+				
+			# last char of speed sequence
 			if char_fx.env.get("_ct", -1) == char_fx.relative_index:
-				caller._block_speed(char_fx.env.get("_base", 1))
+				caller._block_speed(1)
+		
+		# reset for reverse mode
 		if (caller != null 
 				and caller.speed < 0 
 				and char_fx.relative_index == 0 
 				and caller.visible_characters == char_fx.absolute_index - 1):
-			caller._block_speed(char_fx.env.get("_base", 1))
+			
+			caller._block_speed(1)
+			
+		# character counting, so it knows where to start and stop the speed
 		char_fx.env["_ct"] = max(char_fx.relative_index, char_fx.env.get("_ct", 0))
+		
 		return true
